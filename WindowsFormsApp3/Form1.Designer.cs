612f6704d37e4dd75bb9882f18bce72ab32ca744
김용주 <dydrkfsla98@naver.com>
2022-08-01 07:56:58 +0900

    second commit

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -28,18 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea12 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend12 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series23 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series24 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title12 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.lblHelloWorld = new System.Windows.Forms.Label();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -64,36 +77,36 @@
             // 
             // chart1
             // 
-            chartArea12.BorderWidth = 5;
-            chartArea12.Name = "legend 1";
-            this.chart1.ChartAreas.Add(chartArea12);
-            legend12.Name = "Legend1";
-            this.chart1.Legends.Add(legend12);
+            chartArea1.BorderWidth = 5;
+            chartArea1.Name = "legend 1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 44);
             this.chart1.Name = "chart1";
-            series23.BorderWidth = 5;
-            series23.ChartArea = "legend 1";
-            series23.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series23.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            series23.Legend = "Legend1";
-            series23.Name = "데이터 1";
-            series23.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
-            series23.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
-            series24.ChartArea = "legend 1";
-            series24.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series24.Color = System.Drawing.Color.Red;
-            series24.Legend = "Legend1";
-            series24.Name = "데이터 2";
-            this.chart1.Series.Add(series23);
-            this.chart1.Series.Add(series24);
+            series1.BorderWidth = 5;
+            series1.ChartArea = "legend 1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            series1.Legend = "Legend1";
+            series1.Name = "데이터 1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series2.ChartArea = "legend 1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "데이터 2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(358, 221);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "chart1";
-            title12.Font = new System.Drawing.Font("맑은 고딕", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            title12.ForeColor = System.Drawing.Color.Blue;
-            title12.Name = "Title1";
-            title12.Text = "타이틀 제목";
-            this.chart1.Titles.Add(title12);
+            title1.Font = new System.Drawing.Font("맑은 고딕", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.ForeColor = System.Drawing.Color.Blue;
+            title1.Name = "Title1";
+            title1.Text = "타이틀 제목";
+            this.chart1.Titles.Add(title1);
             this.chart1.Click += new System.EventHandler(this.chart1_Click_1);
             // 
             // fileSystemWatcher1
@@ -104,7 +117,7 @@
             // textBox1
             // 
             this.textBox1.AllowDrop = true;
-            this.textBox1.Location = new System.Drawing.Point(137, 281);
+            this.textBox1.Location = new System.Drawing.Point(12, 271);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(466, 25);
             this.textBox1.TabIndex = 3;
@@ -112,11 +125,88 @@
             this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
             this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 322);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(179, 184);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(386, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "한줄씩읽기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(376, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "전체 읽기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(405, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(596, 25);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            chartArea3.Name = "ChartArea2";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.ChartAreas.Add(chartArea3);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(612, 138);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "ecg";
+            series3.XAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series3.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            series4.ChartArea = "ChartArea2";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.LabelForeColor = System.Drawing.Color.Red;
+            series4.Legend = "Legend1";
+            series4.Name = "ppg";
+            series4.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart2.Series.Add(series3);
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(658, 427);
+            this.chart2.TabIndex = 8;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 577);
+            this.ClientSize = new System.Drawing.Size(1294, 577);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.lblHelloWorld);
@@ -126,6 +216,7 @@
             this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,6 +229,12 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.IO.FileSystemWatcher fileSystemWatcher1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -8,14 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
-
+using System.IO;        //추가
+ 
 
 namespace WindowsFormsApp3
 {
     public partial class Form1 : Form
     {
+        //파일 드래그해서 
         private string stFolderPath1;
         private System.IO.DirectoryInfo di;
+
+
+        //그래프
+        int Min = Int32.MaxValue;
+        int Max = Int32.MinValue;
+        int Min2 = Int32.MaxValue;
+        int Max2 = Int32.MinValue;
+        double[] ecg = new double[100000];
+        double[] ppg = new double[100000];
+        private int ecgLength;
+        private int ppgLength;
+        private int Tick = 0;
+        private bool isTimerOn = true;
 
         public Form1()
         {
@@ -46,6 +61,7 @@ namespace WindowsFormsApp3
         }
         private void Form1_Load_1(object sender, EventArgs e)
         {
+            /*
             // Series 삭제
             chart1.Series.Clear();
 
@@ -87,8 +103,23 @@ namespace WindowsFormsApp3
             {
                 Chart2.Points.AddXY(i, i);
             }
+            */
+
+            ecgppgRead();
+            chart2.Series["ecg"].BorderWidth = 1;
+            chart2.Series["ppg"].Color = Color.Red;
+            for (int x = 0; x < ecgLength; x += 1)
+            {
+                chart2.Series["ecg"].Points.AddXY((double)x, ecg[x]);
+            }
+            for (int x = 0; x < ppgLength; x += 1)
+            {
+                chart2.Series["ppg"].Points.AddXY((double)x, ppg[x]);
+            }
+            Controls.Add(chart2);
         }//chart 그리기
 
+        //textbox에 폴더를 드래그하면 listbox에 ('.txt'파일 형식의 파일들을 lsit로 보여줌)
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -121,6 +152,123 @@ namespace WindowsFormsApp3
             }
             else
                 e.Effect = DragDropEffects.None;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //textbox에 폴더를 드래그하면 listbox에 ('.txt'파일 형식의 파일들을 lsit로 보여줌)
+
+
+        //file의 값을 읽어오는 코드
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader SR = new StreamReader("C:\\Users\\dydrk\\source\\repos\\WindowsFormsApp3\\WindowsFormsApp3\\test_log.txt");    //읽어들일 TXT 파일의 경로를 
+                                                                    //매개변수로 StreamReader 생성
+
+            string line;                                            //한 줄씩 읽은 후, 그 값을 저장시킬 변수
+            bool check = false;
+            string result = "";                                     //전체 라인을 저장시킬 변수
+            while ((line = SR.ReadLine()) != null)                  //line변수에 SR에서 한줄을 읽은 걸 저장, 읽은 줄이 null이 아닐때까지 반복
+            {
+                if (check == false && line.StartsWith("network statistics:")==true)
+                {
+                    check = true;
+                }
+                else if(check==true && line.StartsWith("packet wakeup events:")!=true)
+                {
+                    result += line;                                     //전체 라인 변수에 그 값을 저장
+                    result += "\r\n";
+                }//표출을 위해서 추가
+                else if(line.StartsWith("packet wakeup events:") == true)
+                {
+                    break;
+                }
+            }
+
+            textBox2.Text = result;                                 //textbox폼에 전체 읽은 문구를 표출
+            SR.Close();                                             //StreamReader를 닫아줌
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamReader SR = new StreamReader("writeTest.txt");    //읽어들일 TXT 파일의 경로를 
+                                                                    //매개변수로 StreamReader 생성
+
+            string result = "";                                     //읽은 TXT파일을 저장시킬 변수
+            result = SR.ReadToEnd();                                //처음부터 끝까지 읽은 후 저장
+
+            textBox2.Text = result;                                 //textbox폼에 전체 읽은 문구를 표출
+            SR.Close();                                             //StreamReader를 닫아줌
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //file의 값을 읽어오는 코드
+
+
+        //txt 파일을 읽어서 그래프 그리기
+        private void chart2_Click(object sender, EventArgs e)
+        {
+            if (isTimerOn)
+            {
+                timer1.Stop(); isTimerOn = false;
+            }
+            else
+            {
+                timer1.Start();
+                isTimerOn = true;
+            }
+        }
+        private void ecgppgRead()
+        {
+            string filename = @"C:\\Users\\dydrk\\source\\repos\\WindowsFormsApp3\\WindowsFormsApp3\\a101.txt";
+            string[] lines = System.IO.File.ReadAllLines(filename);
+            string filename2 = @"C:\\Users\\dydrk\\source\\repos\\WindowsFormsApp3\\WindowsFormsApp3\\PPG.txt";
+            string[] lines2 = System.IO.File.ReadAllLines(filename2);
+            int i = 0;
+            foreach (string line in lines)
+            {
+                ecg[i] = Convert.ToDouble(line);
+                if (ecg[i] < Min)
+                    Min = Convert.ToInt32(ecg[i]);
+                if (ecg[i] > Max)
+                    Max = Convert.ToInt32(ecg[i]); i++;
+            }
+            ecgLength = i;
+            Console.WriteLine("Min = {0}, Max ={1}", Min, Max);
+            i = 0;
+            foreach (string line in lines2)
+            {
+                ppg[i] = Convert.ToDouble(line);
+                if (ppg[i] < Min2) Min2 = Convert.ToInt32(ppg[i]);
+                if (ppg[i] > Max2) Max2 = Convert.ToInt32(ppg[i]);
+                i++;
+            }
+            ppgLength = i;
+            Console.WriteLine("Min2 = {0}, Max2 = {1}", Min2, Max2);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ecgChartSetting(Tick * 10);
+            Tick += 1;
+        }
+        private void ecgChartSetting(int minX)
+        {
+            chart2.ChartAreas["ChartArea1"].AxisX.Minimum = minX;
+            chart2.ChartAreas["ChartArea1"].AxisX.Maximum = minX + 1500;
+            chart2.ChartAreas["ChartArea1"].AxisY.Minimum = Min / 100 * 100;
+            chart2.ChartAreas["ChartArea1"].AxisY.Maximum = Max / 100 * 100;
+            chart2.ChartAreas["ChartArea2"].AxisX.Minimum = minX;
+            chart2.ChartAreas["ChartArea2"].AxisX.Maximum = minX + 1500;
+            chart2.ChartAreas["ChartArea2"].AxisY.Minimum = Min2;
+            chart2.ChartAreas["ChartArea2"].AxisY.Maximum = Max2; ;
         }
     }
 }
